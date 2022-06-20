78ac1aa714cd610b2c42e2ca15bdee44910bcbcd
Dmitrii Ivanov <idma88@yandex.ru>
2022-06-20 13:29:35 +0300

    Updated general include list

--- Test/Start.h
+++ Test/Start.h
@@ -3,24 +3,25 @@
 #include "gtest/gtest.h"
 using namespace std;
 
-#include <algorithm>     // Algorithms library
-#include <array>         // std::array
-#include <cmath>         // Common mathematical functions
-#include <deque>         // std::deque
-#include <ios>           // Input/Output library
-#include <iostream>      // Input/Output library (iostream)
-#include <istream>       // Input/Output library (istream)
-#include <list>          // std::list, std::forward_list
-#include <map>           // std::map, std::multimap
-#include <optional>      // std::optional
-#include <ostream>       // Input/Output library (ostream)
-#include <queue>         // std::queue, std::priority_queue
-#include <set>           // std::set, std::multiset
-#include <sstream>       // Input/Output library (stringstream)
-#include <stack>         // std::stack
-#include <string>        // Strings library
-#include <tuple>         // std::tuple
-#include <unordered_map> // std::unordered_map, std::unordered_multimap
-#include <unordered_set> // std::unordered_set, std::unordered_multiset
-#include <utility>       // General utility library
-#include <vector>        // std::vector
+#include <algorithm>        // Algorithms library
+#include <array>            // std::array
+#include <cmath>            // Common mathematical functions
+#include <deque>            // std::deque
+#include <initializer_list> // std::initializer_list
+#include <ios>              // Input/Output library
+#include <iostream>         // Input/Output library (iostream)
+#include <istream>          // Input/Output library (istream)
+#include <list>             // std::list, std::forward_list
+#include <map>              // std::map, std::multimap
+#include <optional>         // std::optional
+#include <ostream>          // Input/Output library (ostream)
+#include <queue>            // std::queue, std::priority_queue
+#include <set>              // std::set, std::multiset
+#include <sstream>          // Input/Output library (stringstream)
+#include <stack>            // std::stack
+#include <string>           // Strings library
+#include <tuple>            // std::tuple
+#include <unordered_map>    // std::unordered_map, std::unordered_multimap
+#include <unordered_set>    // std::unordered_set, std::unordered_multiset
+#include <utility>          // General utility library
+#include <vector>           // std::vector
