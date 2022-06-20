1cc49384d40d56a0b991f97f28198a0b77cc4929
Dmitrii Ivanov <idma88@yandex.ru>
2022-06-20 13:30:40 +0300

    Code cleanup for Solution 0002

--- 0002/Solution.cpp
+++ 0002/Solution.cpp
@@ -10,29 +10,31 @@
  */
 
 class Solution {
-  public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *pNode1 = l1, *pNode2 = l2, *pOut = new ListNode(), *pCur = pOut;
-        int       val1, val2, sum, carry                                   = 0, rest;
+public:
+  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode *pNode1 = l1, *pNode2 = l2, *pOut = new ListNode(), *pCur = pOut;
+    int val1, val2, sum, carry = 0;
 
-        while (pNode1 || pNode2 || carry) {
-            val1 = (pNode1 ? pNode1->val : 0);
-            val2 = (pNode2 ? pNode2->val : 0);
+    while (pNode1 || pNode2 || carry) {
+      val1 = (pNode1 ? pNode1->val : 0);
+      val2 = (pNode2 ? pNode2->val : 0);
 
-            sum   = val1 + val2 + carry;
-            carry = sum / 10;
-            sum %= 10;
+      sum = val1 + val2 + carry;
+      carry = sum / 10;
+      sum %= 10;
 
-            if (pNode1) pNode1 = pNode1->next;
-            if (pNode2) pNode2 = pNode2->next;
+      if (pNode1)
+        pNode1 = pNode1->next;
+      if (pNode2)
+        pNode2 = pNode2->next;
 
-            pCur->val = sum;
-            if (pNode1 || pNode2 || carry) {
-                pCur->next = new ListNode();
-                pCur       = pCur->next;
-            }
-        }
-
-        return pOut;
+      pCur->val = sum;
+      if (pNode1 || pNode2 || carry) {
+        pCur->next = new ListNode();
+        pCur = pCur->next;
+      }
     }
+
+    return pOut;
+  }
 };
